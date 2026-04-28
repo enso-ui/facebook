d093a536a3a47e277ffa2862bd1e7170ac9a29c7
mishu <mishu@xtelecom.ro>
2026-04-28 18:12:30 +0300

    Register package icons

--- src/icons.js
+++ src/icons.js
@@ -1,5 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+import { faUserCog } from '@fortawesome/pro-duotone-svg-icons';
 
-library.add(faSquareFacebook, faPuzzlePiece);
+library.add(faSquareFacebook, faPuzzlePiece, faUserCog);
